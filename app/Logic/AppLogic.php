49740a21a29b26b596b85803505cae4b16bd55c7
Kodesmeden <info@kodesmeden.dk>
2024-08-20 14:43:53 +0200

    Update

--- app/Logic/AppLogic.php
+++ app/Logic/AppLogic.php
@@ -10,20 +10,31 @@ class AppLogic
     protected $thisPhpVersion;
     protected $phpVersions;
     protected $webserverUser;
+    protected $webserverGroup;
     protected $websitePath = '/var/www/resources/websites';
     protected $sitesAvailable = '/etc/nginx/sites-available';
     protected $sitesEnabled = '/etc/nginx/sites-enabled';
 
+    protected $appName;
+    protected $appDomain;
+    protected $phpSock;
+    protected $database;
+    protected $installer;
+
     // TODO: Add variables for name, domain, databaseLogic etc.
 
-    public function __construct()
+    public function __construct(DatabaseLogic $databaseLogic, InstallerLogic $installerLogic)
     {
         $this->defaultPhpVersion = env('DEFAULT_PHP_VERSION');
         $this->thisPhpVersion = $this->defaultPhpVersion;
         $this->phpVersions = array_map('basename', glob('/etc/php/*'));
         $this->webserverUser = env('WEBSERVER_USER');
+        $this->webserverGroup = env('WEBSERVER_GROUP');
+        $this->database = $databaseLogic;
+        $this->installer = $installerLogic;
 
         // TODO: Add support for databaseLogic
+        // TODO: Add support for installerLogic
         // TODO: Add support for website ID + set website info
     }
 
@@ -37,26 +48,33 @@ class AppLogic
 
     public function createApp( $name, $type, $phpVersion = null, $repo = null) {
         $type = ucfirst($type);
-        $callable = "create{$type}App";
+        $create = "create{$type}App";
+        $install = "install{$type}";
         $this->thisPhpVersion = $phpVersion ?? $this->defaultPhpVersion;
+        $this->appName = $this->getFormattedName($name);
+        $this->appDomain = $this->getDomainFromName($this->appName);
+
+        $phpSockName = Str::slug($name, '');
+        $this->phpSock = "/run/php/php{$this->thisPhpVersion}-{$phpSockName}.sock";
 
         try{
-            return $this->$callable($name, $repo);
+            $this->addPhpConfig($this->appDomain);
+            $this->addSymbolicLink($this->appDomain);
+            $this->addWebsiteFolder($this->appDomain);
+            $this->installer->$install("{$this->websitePath}/{$this->appDomain}");
+            $this->resetPermissions($this->appDomain);
+            $this->updateHostsFile();
+            $this->restartServices();
+            // TODO: Add database creation here
+            return $this->$create($repo);
         } catch (\Exception $e) {
             dd($e->getMessage());
         }
     }
 
-    private function createPhpApp($name) {
-        $name = $this->getFormattedName($name);
-        $domain = $this->getDomainFromName($name);
-
+    private function createPhpApp() {
         try{
-            $this->addSiteConf($domain);
-            $this->addPhpConfig($domain);
-            $this->addSymbolicLink($domain);
-            $this->addWebsiteFolder($domain);
-            $this->restartServices();
+            $this->addSiteConf($this->appDomain);
         } catch (\Exception $e) {
             throw $e;
         }
@@ -64,21 +82,28 @@ class AppLogic
         return true;
     }
 
-    private function createLaravelApp($name) {
-        $name = $this->getFormattedName($name);
-        $domain = $this->getDomainFromName($name);
+    private function createLaravelApp() {
+        try{
+            $this->addSiteConf($this->appDomain, true);
+        } catch (\Exception $e) {
+            throw $e;
+        }
+
+        return true;
     }
 
-    private function createWordpressApp($name) {
-        $name = $this->getFormattedName($name);
-        $domain = $this->getDomainFromName($name);
+    private function createWordpressApp() {
+        try{
+            $this->addSiteConf($this->appDomain);
+        } catch (\Exception $e) {
+            throw $e;
+        }
+
+        return true;
     }
 
-    private function createGitApp($name, $repo)
+    private function createGitApp($repo)
     {
-        $name = $this->getFormattedName($name);
-        $domain = $this->getDomainFromName($name);
-
         if (substr($repo, -4) !== '.git') {
             return false;
         }
@@ -96,17 +121,19 @@ class AppLogic
 
         $gitUrl = str_replace('https://', "https://$gitUser:$gitPass@", $repo);
 
-        exec("cd {$this->websitePath}/ && git clone $gitUrl $domain 2>&1", $output, $errorCode);
+        exec("cd {$this->websitePath}/ && git clone {$gitUrl} {$this->appDomain} 2>&1", $output, $errorCode);
 
         return (bool) empty($errorCode);
     }
 
-    private function addSiteConf($domain) {
+    private function addSiteConf($domain, $laravel = false) {
         $path = "/etc/nginx/sites-available/{$domain}";
+        $root = $laravel ? "/var/www/resources/websites/{$domain}/public" : "/var/www/resources/websites/{$domain}";
+
         $content = 'server {
     listen 80;
     server_name ' . $domain . ';
-    root /var/www/resources/websites/' . $domain . ';
+    root ' . $root . ';
 
     index index.php;
 
@@ -134,18 +161,19 @@ class AppLogic
         $this->createSystemFile($path, $content);
     }
 
-    private function removeSiteConfig($websiteId, $php) {
+    private function removeSiteConfig($websiteId) {
         // $path = "/etc/nginx/sites-available/{$domain}";
+        // $path = "/etc/nginx/sites-enabled/{$domain}";
     }
 
     private function addPhpConfig($domain) {
         $path = "/etc/php/{$this->thisPhpVersion}/fpm/pool.d/{$domain}.conf";
         $content = "[{$domain}]
 user = {$this->webserverUser}
-group = {$this->webserverUser}
-listen = /run/php/php{$this->thisPhpVersion}.sock
+group = {$this->webserverGroup}
+listen = {$this->phpSock}
 listen.owner = {$this->webserverUser}
-listen.group = {$this->webserverUser}
+listen.group = {$this->webserverGroup}
 pm = dynamic
 pm.max_children = 50
 pm.start_servers = 5
@@ -160,7 +188,7 @@ pm.max_spare_servers = 35";
     }
 
     private function addSymbolicLink($domain) {
-        $confPath = "/etc/nginx/sites-available/{$domain}";
+        $confPath = escapeshellarg("/etc/nginx/sites-available/{$domain}");
         $symbolicLinkCmd = "ln -s {$confPath} /etc/nginx/sites-enabled/";
 
         return $this->runCommand($symbolicLinkCmd);
@@ -174,15 +202,57 @@ pm.max_spare_servers = 35";
     // }
 
     private function addWebsiteFolder($domain) {
-        $websitePath = "{$this->websitePath}/{$domain}";
+        $websitePath = escapeshellarg("{$this->websitePath}/{$domain}");
         $createFolderCmd = "mkdir -p {$websitePath}";
 
+        // return $createFolderCmd;
         return $this->runCommand($createFolderCmd);
     }
 
-    private function restartServices() {
+    private function resetPermissions() {
+        $command = "sudo chown -R {$this->webserverUser}:{$this->webserverGroup} {$this->websitePath}";
+        exec($command . " 2>&1", $output, $copyErrorCode);
+
+        // Fix general permissions for files and folders
+        exec("sudo find {$this->websitePath} -type d -exec chmod 755 {} \; > /dev/null 2>&1 &");
+        exec("sudo find {$this->websitePath} -type f -exec chmod 644 {} \; > /dev/null 2>&1 &");
+
+        if ($copyErrorCode !== 0) {
+            throw new \Exception("Failed to set permissions. Return code: $copyErrorCode\n");
+        }
+
+        return true;
+    }
+
+    public function updateHostsFile() {
+        $hostsFile = '/etc/hosts';
+
+        $hostsContent  = "127.0.0.1 localhost\n";
+        $hostsContent .= "127.0.0.1 pma.test\n";
+        $hostsContent .= "127.0.0.1 dev.test\n";
+
+        $websites = array_map('basename', glob("{$this->websitePath}/*"));
+
+        foreach ($websites as $website) {
+            $hostsContent .= "127.0.0.1 {$website}\n";
+        }
+
+        $command = "echo " . escapeshellarg($hostsContent) . " | sudo tee " . escapeshellarg($hostsFile);
+        exec($command . " 2>&1", $output, $errorCode);
+
+        if ($errorCode !== 0) {
+            throw new \Exception("Failed to update hosts file. Return code: $errorCode\n");
+        }
+
+        return true;
+    }
+
+    public function restartServices() {
         // TODO: Loop through all PHP versions and add to command
-        $restartCmd = "service php{$this->thisPhpVersion}-fpm restart && service mysql restart && systemctl restart systemd-resolved && systemctl restart nginx && systemctl restart NetworkManager";
+        // systemctl restart systemd-resolved && chown -R www-data:www-data /usr/share/phpmyadmin && chown -R kodesmeden:www-data /var/www && find /var/www -type d -exec chmod 755 {} \; && find /var/www -type f -exec chmod 644 {} \; && find /var/www -type d -exec chmod 2755 {} +
+        // $restartCmd = "sudo service php{$this->thisPhpVersion}-fpm restart && sudo service mysql restart && sudo systemctl restart systemd-resolved && sudo systemctl restart nginx && sudo systemctl restart NetworkManager";
+        // $restartCmd = "sudo service php{$this->thisPhpVersion}-fpm restart && sudo service mysql restart && sudo systemctl restart systemd-resolved && sudo systemctl restart nginx";
+        $restartCmd = "sudo systemctl reload php{$this->thisPhpVersion}-fpm && sudo systemctl reload systemd-resolved && sudo systemctl reload nginx";
 
         return exec($restartCmd);
     }
@@ -192,14 +262,14 @@ pm.max_spare_servers = 35";
         $tempFile = tempnam(sys_get_temp_dir(), 'nginx_site_');
         file_put_contents($tempFile, $content);
 
-        $command = "echo " . escapeshellarg(env('SUDO_PASSWORD')) . " | sudo -S cp " . escapeshellarg($tempFile) . " " . escapeshellarg($path);
+        $command = "sudo cp " . escapeshellarg($tempFile) . " " . escapeshellarg($path);
         exec($command . " 2>&1", $output, $copyErrorCode);
 
         if ($copyErrorCode === 0) {
-            $command = "echo " . escapeshellarg(env('SUDO_PASSWORD')) . " | sudo -S chmod 644 " . escapeshellarg($path);
+            $command = "sudo chmod 644 " . escapeshellarg($path);
             exec($command . " 2>&1", $output, $chmodErrorCode);
 
-            if ($copyErrorCode !== 0) {
+            if ($chmodErrorCode !== 0) {
                 $error = "Failed to set permissions. Return code: $chmodErrorCode\n";
             }
         } else {
@@ -216,7 +286,7 @@ pm.max_spare_servers = 35";
     }
 
     private function runCommand($command) {
-        $command = "echo " . escapeshellarg(env('SUDO_PASSWORD')) . " | sudo -S " . escapeshellarg($command);
+        $command = "sudo " . $command;
         exec($command . " 2>&1", $output, $errorCode);
 
         return (bool) empty($errorCode);
---
+++ app/Logic/InstallerLogic.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Logic;
+
+class InstallerLogic
+{
+    public function __construct()
+    {
+        
+    }
+
+    public function installPhp($path) {
+        // TODO: Create simple index.php file
+    }
+
+    public function installLaravel($path) {
+        // TODO: Create and setup Laravel project
+    }
+
+    public function installWordpress($path) {
+        // TODO: Create and setup Wordpress project
+    }
+}
